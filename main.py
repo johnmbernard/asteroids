f1504ddc4bab359f2176e8c110ead8c9c20bcd48
Beastnard <john.m.bernard@outlook.com>
2024-12-30 13:22:06 -0500

    debugged if statements in main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
 
+print("Starting asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
+
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pass
 
 if __name__ == "__main__":
     main()
